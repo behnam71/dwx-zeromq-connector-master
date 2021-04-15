591490f0b2d3bb6b8ca50db84b60ceb8498d03bd
behnam heydari <behnamheydari.7.21@gmail.com>
2021-04-15 21:58:41 +0430

    Update df_concat.py

--- data_tickstory/df_concat.py
+++ data_tickstory/df_concat.py
@@ -3,7 +3,7 @@ import datetime
 import os
 import glob
 
-Dir_Path = 'C:\\Users\\BEHNAMH721AS.RN\\Documents'
+Dir_Path = 'C:\\Users\\Administrator\\Documents\\M1\\forex_data'
 
 
 
@@ -12,23 +12,22 @@ def FileName(path):
     result = glob.glob('*.csv')
     return result
 
-result = FileName(Dir_Path)
-_start = 0
+_result = FileName(Dir_Path)
 _HIST_DATA_DF = pd.DataFrame()
-for i in range(0, len(result)):
-	df = pd.read_csv(result[i], sep=',', low_memory=False)
-	_HIST_DATA_DF = _HIST_DATA_DF.append(df)
-	_HIST_DATA_DF = _HIST_DATA_DF.reset_index(drop=True)
-	_HIST_DATA_DF.loc[_start:len(_HIST_DATA_DF), "tic"] = result[i].split(".")[0]
-	_start += len(_HIST_DATA_DF)
+_S = 0
+for i in range(0, len(_result)):
+    _df = pd.read_csv(_result[i], sep=',', low_memory=False)
+    _F = _S + len(_df)
+    _HIST_DATA_DF = _HIST_DATA_DF.append(_df)
+    _HIST_DATA_DF = _HIST_DATA_DF.reset_index(drop=True)
+    _HIST_DATA_DF.loc[_S:_F, "tic"] = _result[i].split(".")[0]
+    _S += len(_df)
 
 for i in range(0, len(_HIST_DATA_DF)):
-    _HIST_DATA_DF.loc[i, 'date'] = datetime.datetime.strptime(str(_HIST_DATA_DF.loc[i, "date"]), '%Y%m%d %H:%M:%S')
+    _HIST_DATA_DF.loc[i, 'date'] = datetime.datetime.strptime(_HIST_DATA_DF.loc[i, "date"], '%Y%m%d %H:%M:%S')
     _HIST_DATA_DF.loc[i, 'volume'] = '{:.1f}'.format(int(_HIST_DATA_DF.loc[i, 'volume']))
 
-#_HIST_DATA_DF = _HIST_DATA_DF.sort_values(by=['date','tic']).reset_index(drop=True)
+_HIST_DATA_DF = _HIST_DATA_DF.sort_values(by=['date','tic']).reset_index(drop=True)
 
-_HIST_DATA_DF.to_csv("mt4_dataset_o.csv", float_format='%6.5f')
+_HIST_DATA_DF.to_csv("file.csv", float_format='%6.5f')
 print(_HIST_DATA_DF)
-
-
